6a4226b2fb8f5837184795ad6099108d14c212c8
gabtomas <46448735+gabtomas@users.noreply.github.com>
2023-11-04 16:11:14 +0100

    delete unnecessary code

--- packages/backend/src/config/app.config.ts
+++ packages/backend/src/config/app.config.ts
@@ -1,7 +1,7 @@
 export default () => ({
   db: {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT as unknown as number,
+    port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
